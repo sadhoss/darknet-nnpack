9fb749dbc2c18335e7fc6614eaa401df7fb4a5af
Sadegh Hosseinpoor <16901477+sadhoss@users.noreply.github.com>
2020-03-25 19:07:53 +0100

    terminology change

--- mug_glass_detector/track_boxes.py
+++ mug_glass_detector/track_boxes.py
@@ -8,7 +8,7 @@
 #
 # Resolve all boxes from string
 def get_boxes(input_string, frame_res):
-    all_boxes = []
+    detected_boxes = []
 
     for n in input_string.split('\n'):
         if 'Box ' in n:
@@ -36,9 +36,9 @@ def get_boxes(input_string, frame_res):
             box['w'] = box['w'] * frame_res[0]
             box['h'] = box['h'] * frame_res[1]
 
-            all_boxes.append(box)
+            detected_boxes.append(box)
 
-    return all_boxes
+    return detected_boxes
 
 
 
@@ -46,12 +46,26 @@ def get_boxes(input_string, frame_res):
 def track_boxes(input_string, frame_res):
 
     tracked_boxes = {
-    'center_coordinates' : [],
-    'label' : [],
-    'counter' : [],
-    'ticks' : []
-    }
+        'center_coordinates' : [],
+        'label' : [],
+        'counter' : [],
+        'ticks' : []
+        }
 
-    new_boxes = get_boxes(input_string, frame_res)
+    detected_boxes = get_boxes(input_string, frame_res)
+    print('TEST - Printing box information: ')
+    print(detected_boxes)
+
+    # detected boxes
+    if detected_boxes:
+        # there are boxes we are tracking already
+        if tracked_boxes['center_coordinates']:
+            pass
+        # new boxes we have not tracked yet
+        else:
+            pass
+    # no new boxes
+    else: 
 
     
+
